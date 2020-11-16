464a655cb1701db7621e5581b3445ca20276cba2
Calaway <calaway@users.noreply.github.com>
2020-11-15 19:46:48 -0700

    Add assertions for remaining links and link order

--- cypress/e2e/header.spec.js
+++ cypress/e2e/header.spec.js
@@ -1,9 +1,23 @@
 describe('Header', () => {
-  it('contains the correct links', () => {
+  it('contains the correct links in the correct order', () => {
     cy.visit('/');
+
+    cy.findAllByRole('navigation').contains('conduit Home Sign in Sign up');
 
     cy.findAllByRole('navigation')
       .contains('conduit')
       .should('have.attr', 'href', '#/');
+
+    cy.findAllByRole('navigation')
+      .contains('Home')
+      .should('have.attr', 'href', '#/');
+
+    cy.findAllByRole('navigation')
+      .contains('Sign in')
+      .should('have.attr', 'href', '#/login');
+
+    cy.findAllByRole('navigation')
+      .contains('Sign up')
+      .should('have.attr', 'href', '#/register');
   });
 });
